a394aad14b8abea4e3fb37d58368539f8921db74
k <kiaraanthonyp@gmail.com>
2025-03-27 19:01:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function openEnvelope() {
+    document.getElementById("envelope").classList.add("open");
+    setTimeout(() => {
+        document.getElementById("message").style.display = "block";
+        setInterval(createHeart, 500);
+    }, 1000);
+}
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    document.body.appendChild(heart);
+    
+    const randomX = Math.random() * window.innerWidth;
+    heart.style.left = `${randomX}px`;
+    
+    setTimeout(() => {
+        heart.remove();
+    }, 3000);
+} 
